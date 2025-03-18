53a4d549e6071d1f31197202e95a6c335429dd48
Jose G Nieto <jose.nieto@noblenex.us>
2025-03-18 10:37:58 -0500

    mongocc@1.1.0 simplified

--- mongocc.go
+++ mongocc.go
@@ -8,27 +8,22 @@ import (
 	"go.mongodb.org/mongo-driver/v2/mongo/options"
 )
 
-func Connect(mongoUri string, dbName string, dataStore *MongoDataStore) error {
+func Connect(mongoUri string, dbName string) (*mongo.Database, error) {
 
 	client, err := mongo.Connect(options.Client().ApplyURI(mongoUri))
 	if err != nil {
-		return err
+		return nil, err
 	}
+
+	ctx := context.TODO()
 
 	// Send a ping to confirm a successful connection
 	var result bson.M
-	if err := client.Database("admin").RunCommand(context.TODO(), bson.D{{"ping", 1}}).Decode(&result); err != nil {
-		return err
+	if err := client.Database("admin").RunCommand(ctx, bson.D{{"ping", 1}}).Decode(&result); err != nil {
+		return nil, err
 	}
 	fmt.Printf("You successfully connected to MongoDB://%s\n", dbName)
 
-	dataStore.Client = client
-	dataStore.DB = client.Database(dbName)
+	return client.Database(dbName), nil
 
-	return nil
-
-}
-
-func (ds *MongoDataStore) GetColl(collectionName string) *mongo.Collection {
-	return ds.DB.Collection(collectionName)
 }
--- structs.go
+++
@@ -1,8 +0,0 @@
-package mongocc
-
-import "go.mongodb.org/mongo-driver/v2/mongo"
-
-type MongoDataStore struct {
-	DB     *mongo.Database
-	Client *mongo.Client
-}
