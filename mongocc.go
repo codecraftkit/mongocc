daf7043a93f87e1833596f817eea5563bc7971b8
Jose G Nieto <jose.nieto@noblenex.us>
2025-10-04 17:34:36 -0500

    v1.2.11

--- mongocc.go
+++ mongocc.go
@@ -58,50 +58,86 @@ func (mongodb *MongoQueries) GetCollection(collectionName string) *mongo.Collect
 }
 
 func (mongodb *MongoQueries) Find(ctx context.Context, collectionName string, query interface{}, opts *options.FindOptionsBuilder) (*mongo.Cursor, error) {
+	if mongodb.Debug {
+		fmt.Println("[LOG] Find", collectionName, query, opts)
+	}
 	return mongodb.db.Collection(collectionName).Find(ctx, query, opts)
 }
 
 func (mongodb *MongoQueries) FindOne(ctx context.Context, collectionName string, query interface{}, opts *options.FindOneOptionsBuilder) *mongo.SingleResult {
+	if mongodb.Debug {
+		fmt.Println("[LOG] FindOne", collectionName, query, opts)
+	}
 	return mongodb.db.Collection(collectionName).FindOne(ctx, query, opts)
 }
 
 func (mongodb *MongoQueries) FindOneAndUpdate(ctx context.Context, collectionName string, query interface{}, update interface{}, opts *options.FindOneAndUpdateOptionsBuilder) *mongo.SingleResult {
+	if mongodb.Debug {
+		fmt.Println("[LOG] FindOneAndUpdate", collectionName, query, update, opts)
+	}
 	return mongodb.db.Collection(collectionName).FindOneAndUpdate(ctx, query, update, opts)
 }
 
 func (mongodb *MongoQueries) InsertOne(ctx context.Context, collectionName string, document interface{}) (*mongo.InsertOneResult, error) {
+	if mongodb.Debug {
+		fmt.Println("[LOG] InsertOne", collectionName, document)
+	}
 	return mongodb.db.Collection(collectionName).InsertOne(ctx, document)
 }
 
 func (mongodb *MongoQueries) InsertMany(ctx context.Context, collectionName string, documents []interface{}) (*mongo.InsertManyResult, error) {
+	if mongodb.Debug {
+		fmt.Println("[LOG] InsertMany", collectionName, documents)
+	}
 	return mongodb.db.Collection(collectionName).InsertMany(ctx, documents)
 }
 
 func (mongodb *MongoQueries) UpdateOne(ctx context.Context, collectionName string, query interface{}, update interface{}, opts *options.UpdateOneOptionsBuilder) (*mongo.UpdateResult, error) {
+	if mongodb.Debug {
+		fmt.Println("[LOG] UpdateOne", collectionName, query, update, opts)
+	}
 	return mongodb.db.Collection(collectionName).UpdateOne(ctx, query, update, opts)
 }
 
 func (mongodb *MongoQueries) UpdateMany(ctx context.Context, collectionName string, query interface{}, update interface{}, opts *options.UpdateManyOptionsBuilder) (*mongo.UpdateResult, error) {
+	if mongodb.Debug {
+		fmt.Println("[LOG] UpdateMany", collectionName, query, update, opts)
+	}
 	return mongodb.db.Collection(collectionName).UpdateMany(ctx, query, update, opts)
 }
 
 func (mongodb *MongoQueries) DeleteOne(ctx context.Context, collectionName string, query interface{}, opts *options.DeleteOneOptionsBuilder) (*mongo.DeleteResult, error) {
+	if mongodb.Debug {
+		fmt.Println("[LOG] DeleteOne", collectionName, query, opts)
+	}
 	return mongodb.db.Collection(collectionName).DeleteOne(ctx, query, opts)
 }
 
 func (mongodb *MongoQueries) DeleteMany(ctx context.Context, collectionName string, query interface{}, opts *options.DeleteManyOptionsBuilder) (*mongo.DeleteResult, error) {
+	if mongodb.Debug {
+		fmt.Println("[LOG] DeleteMany", collectionName, query, opts)
+	}
 	return mongodb.db.Collection(collectionName).DeleteMany(ctx, query, opts)
 }
 
 func (mongodb *MongoQueries) Aggregate(ctx context.Context, collectionName string, pipeline interface{}, opts *options.AggregateOptionsBuilder) (*mongo.Cursor, error) {
+	if mongodb.Debug {
+		fmt.Println("[LOG] Aggregate", collectionName, pipeline, opts)
+	}
 	return mongodb.db.Collection(collectionName).Aggregate(ctx, pipeline, opts)
 }
 
 func (mongodb *MongoQueries) CountDocuments(ctx context.Context, collectionName string, query interface{}) (int64, error) {
+	if mongodb.Debug {
+		fmt.Println("[LOG] CountDocuments", collectionName, query)
+	}
 	return mongodb.db.Collection(collectionName).CountDocuments(ctx, query)
 }
 
-func CheckMongoError(err error) error {
+func (mongodb *MongoQueries) CheckMongoError(err error) error {
+	if mongodb.Debug {
+		fmt.Println("[LOG] CheckMongoError", err)
+	}
 	if err != nil {
 		if errors.Is(err, mongo.ErrNoDocuments) {
 			return fmt.Errorf("NOT_FOUND: %s", err.Error())
--- structs.go
+++ structs.go
@@ -1,10 +1,5 @@
 package mongocc
 
-import "go.mongodb.org/mongo-driver/v2/bson"
-
-type Options struct {
-	Fields *[]string
-	Limit  *int64
-	Skip   *int64
-	Sort   *[]bson.M
+type ClientOptions struct {
+	Debug bool
 }
